2bea256eebf0a31172703b0258cd2194a147a7c8
ankit <ankit@DESKTOP-A4EU1SK>
2020-07-13 22:14:06 +0530

    Changes

--- Spring BeanFactoryPostProcessor/src/org/Spring/Shape.java
+++ Spring BeanFactoryPostProcessor/src/org/Spring/Shape.java
@@ -6,7 +6,7 @@ import org.springframework.core.SpringVersion;
 
 @SuppressWarnings("deprecation")
 public class Shape {
-
+//In this project spirng.xml place holder is been used for an example of bean factory Post processor
 	public static void main(String[] args) {
 		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
 		Triangle triangle=(Triangle) context.getBean("triangle");
